eb8e23076d211593576819c5292a4a40f458689d
AsyaRain <asya93.07@gmail.com>
2024-01-23 18:09:02 +0300

    добавлено задание 6

---
+++ ex6.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://playground.learnqa.ru/api/long_redirect"
+response = requests.get(url, allow_redirects=True)
+
+redirect_count = len(response.history)
+final_url = response.url
+
+print(f"Количество редиректов: {redirect_count}")
+print(f"Итоговый URL: {final_url}")
